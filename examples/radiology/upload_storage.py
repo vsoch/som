3f6e4e4b79267b8581d4fbe18945ced1f4a2dc48
vsoch <vsochat@stanford.edu>
2017-03-09 18:50:26 -0500

    modified:   examples/radiology/upload_storage.py

--- examples/radiology/upload_storage.py
+++ examples/radiology/upload_storage.py
@@ -2,13 +2,18 @@
 
 # This is an example script to upload data (images, text, metadata) to
 # google cloud storage and datastore. We use the wordfish standard,
-# assuming the data has been de-identified.
+# assuming the data has been de-identified. The first step 
+# below we structure our dataset. This also validates that it's
+# formatted correctly. In the second step, we use the radiology
+# client to upload the structures to storage and datastore.
+
 
 from som.storage.google.radiology import Client
 from som.wordfish.structures import structure_dataset
 
+
 compressed_data = '../../../wordfish-standard/demo/cookies.zip'
 structures = structure_dataset(compressed_data)
 
-client = Client()
-response = client.upload(structures)
+radiology_client = Client()
+response = radiology_client.upload(structures)
